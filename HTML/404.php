62cf632996c344ab2c432e67c301110c6a2c729a
Adam Davies <45388618+Alvin76@users.noreply.github.com>
2020-02-14 16:29:35 -0500

    added nav to the other pages

--- HTML/404.php
+++ HTML/404.php
@@ -1,2 +1,9 @@
 <p>404 - page not found.</p>
+<nav>
+  <a href="home.html">Home</a>
+  <a href="login.html">login</a>
+  <a href="recipies.html">recipies</a>
+  <a href="shopinglist.html">shoping list</a>
+  <a href="myinventory.html">my fridge</a>
+</nav>
 <p> <img src="Images/Cat_not_found.JPG" alt=""> </p>
