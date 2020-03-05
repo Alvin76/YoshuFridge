7b7005d000b73768585dc95742f46a121ee63823
Evan <ciriel@live.com>
2020-03-05 14:47:45 -0500

    Some CSS for inventory and shopping list. It made sense to me to have them in same format, we will discuss how to further improve this together

--- Content/myinventory.php
+++ Content/myinventory.php
@@ -1,61 +1,61 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>My Fridge</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<html lang="en">
 
-      <!--Fontawesome CDN-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<head>
 
-    <!--Custom styles-->
-    <link rel="stylesheet" type="text/css" href="../CSS/home.css">
-    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet'>
-  </head>
-  <body>
-    <header>My Fridge</header>
-    <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading">Fridge-o-Matic </div>
-      <div class="list-group list-group-flush">
-        <a href="home.php" class="list-group-item list-group-item-action bg-light">  <img src="https://img.icons8.com/dusk/64/000000/home.png" alt="home">
-          Home</a>
-        <a href="recipies.php" class="list-group-item list-group-item-action bg-light"><img src="https://img.icons8.com/dusk/64/000000/rack-of-lamb.png" alt="Recipes">
-          Recipes</a>
-        <a href="myinventory.php" class="list-group-item list-group-item-action bg-light"><img src="https://img.icons8.com/dusk/64/000000/fridge.png" alt ="Inventory">
-          Inventory</a>
-        <a href="shopinglist.php" class="list-group-item list-group-item-action bg-light"><img src="https://img.icons8.com/dusk/64/000000/price-tag.png" alt = "Shopping List">
-          Shopping List</a>
-        <a href="login.php" class="list-group-item list-group-item-action bg-light"><img src="https://img.icons8.com/dusk/64/000000/email.png" alt="Login/Sign Up">
-          Login/Sign Up</a>
-        <a href="theTeam.php" class="list-group-item list-group-item-action bg-light"><img src="https://img.icons8.com/dusk/64/000000/open-source.png">
-          The Team</a>
-          <a class="list-group-item list-group-item-action bg-light" href="" data-toggle="dropdown"><img src="https://img.icons8.com/dusk/64/000000/like.png">
-            Trending<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li class="list-group-item list-group-item-action bg-light"><a href="#"><img src="https://img.icons8.com/dusk/64/000000/summer.png">
-              In Season</a></li>
-            <li class="list-group-item list-group-item-action bg-light"><a href="#"><img src="https://img.icons8.com/dusk/64/000000/flex-biceps.png">
-              Protein Powered</a></li>
-            <li class="list-group-item list-group-item-action bg-light"><a href="#"><img src="https://img.icons8.com/dusk/64/000000/vegan-food.png">
-              Vegan</a></li>
-          </ul>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>My Inventory</title>
+
+  <!--Bootsrap 4 CDN-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+  <!--Custom styles-->
+  <link rel="stylesheet" type="text/css" href="../CSS/scrollingNav.css">
+
+</head>
+
+<body id="page-top">
+
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Fridge-o-Matic</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#fridge">Fridge</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#freezer">Freezer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#pantry">Pantry</a>
+          </li>
+        </ul>
       </div>
     </div>
-    <h1>Your Fridgeator :D</h1>
-    <main>
-      <div class="frozenFood">
-        <h2>Frezer</h2>
-        <!--Add a serch call here for items in the database-->
-        <div class="FrezerSearch">
-          <p>add a item here</p>
-          <input type="text" placeholder="Search..">
-        </div>
-        <p>bacon</p><button type="button">+add</button><button type="button">-remove</button>
-      </div>
-      <div class="fridgeFood">
+  </nav>
+
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>This Section</h1>
+      <p class="lead">Will have some sort of wheel with clickable images for getting to other pages; potentially a carousel, we will discuss it</p>
+    </div>
+  </header>
+
+  <section id="fridge">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
         <h2>Fridge</h2>
         <!--Add a serch call here for items in the database-->
         <div class="FridgeSearch">
@@ -64,8 +64,31 @@
         </div>
         <p>bacon</p><button type="button">+add</button><button type="button">-remove</button>
       </div>
-      <div class="fridgeFood">
-        <h2>pantry</h2>
+        </div>
+      </div>
+  </section>
+
+  <section id="freezer" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+           <h2>Frezer</h2>
+        <!--Add a serch call here for items in the database-->
+        <div class="FrezerSearch">
+          <p>add a item here</p>
+          <input type="text" placeholder="Search..">
+        </div>
+        <p>chicken</p><button type="button">+add</button><button type="button">-remove</button>
+      </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="pantry">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+           <h2>pantry</h2>
         <!--Add a serch call here for items in the database-->
         <div class="pantrySearch">
           <p>add a item here</p>
@@ -73,16 +96,21 @@
         </div>
         <p>rice</p><button type="button">+add</button><button type="button">-remove</button>
       </div>
-      <div class="other">
-        <h2>pantry</h2>
-        <!--Add a serch call here for items in the database-->
-        <div class="otherSearch">
-          <p>add a item here</p>
-          <input type="text" placeholder="Search..">
-        </div>
-        <p>beer</p><button type="button">+add</button><button type="button">-remove</button>
       </div>
-    </main>
-    <footer><small>&copy;<?php echo date('Y'); ?> Team Alpaca </small></footer>
-  </body>
+    </div>
+  </section>
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
 </html>
