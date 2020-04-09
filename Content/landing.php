63c71ae7f5c7f97e3e52f899590baea769524144
Evan <ciriel@live.com>
2020-04-09 17:56:23 -0400

    okay

--- Content/landing.php
+++ Content/landing.php
@@ -188,7 +188,7 @@
               <div class="timeline-panel">
                 <div class="timeline-heading">
                   <h4>Inventory</h4>
-                  <a href="#"><h4 class="subheading">Click Here</h4></a>
+                  <a href="myinventory.php"><h4 class="subheading">Click Here</h4></a>
                 </div>
                 <div class="timeline-body">
                   <p class="text-muted">Upload your fridge contents for your inital profile. From there we rummage and optimizes your ingredients and can even suggest dishes based on whats in YOUR cupboard.</p>
@@ -202,7 +202,7 @@
               <div class="timeline-panel">
                 <div class="timeline-heading">
                   <h4>Shopping List</h4>
-                  <a href="#"><h4 class="subheading">Click Here</h4></a>
+                  <a href="shopinglist.php"><h4 class="subheading">Click Here</h4></a>
                 </div>
                 <div class="timeline-body">
                   <p class="text-muted">Combining the hard recipe planning you did and the virtial contents of your fridge, we create a personalized shopping list tp offer a peace of mind at the market. No forgot ingredients and non need for extra trips to the store! </p>
